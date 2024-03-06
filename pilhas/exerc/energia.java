7ee74b1d7e39bf5dd47f552aa5868c1607f76bf3
Felipe da Costa Ferreira <20231014040005@ifrn.local>
2024-03-06 08:20:27 -0300

    questão da energia solar

--- pilhas/exerc/energia.java
+++ pilhas/exerc/energia.java
@@ -1,21 +1,38 @@
 package exerc;
 import java.util.Scanner;
+import java.util.ArrayList;
 import linkedList.NodeStack;
+//import filas.array.NodeQueue;
+import linkedList.pilha;
 
 public class energia{
     public static void main(String[] args){
         Scanner s = new Scanner(System.in);
-        NodeStack<Object> pilha = new NodeStack<Object>();
-        //NodeStack<Object> pilha = new NodeStack<Object>();
-        for(int i = 0; i < 10; i++){
-            NodeStack<Object> nova_pilha = new NodeStack<Object>();
-            for(int j = 0; j < 10; j++)
-                nova_pilha.push(j+i);
-            pilha.push(nova_pilha);
-        }
-        //  while(s.hasNextInt()){
-        //  }
-        // //     x = ;
-        System.out.println(pilha);
+        
+       ArrayList<Integer> a, novo_a, nova_linha;
+       NodeStack<Object> linhas = new NodeStack<Object>();
+        a = new ArrayList<Integer>();
+        while(s.hasNextInt())
+            a.add(s.nextInt());
+
+        do{
+            novo_a = new ArrayList<Integer>(); // linha antiga
+            nova_linha = new ArrayList<Integer>(); // nova linha formada
+            for(int i = 0; i < a.size()-1; i++){
+                if(a.get(i) < a.get(i+1))
+                    novo_a.add(a.get(i));      
+                nova_linha.add(a.get(i+1));
+            }
+            if(novo_a.size() == 0){ // se a linha antiga estiver ok, o laço vai parar
+                linhas.push(a); 
+                break;
+            } 
+            linhas.push(nova_linha);
+            System.out.println(linhas);
+
+            a = novo_a;
+        }while(a.size() != 0);
+        
+
     }
 }
