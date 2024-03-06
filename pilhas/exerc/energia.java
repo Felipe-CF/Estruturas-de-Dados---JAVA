816474c675b2944f2070f61cc52c0f388a8a8b93
Felipe da Costa Ferreira <20231014040005@ifrn.local>
2024-03-06 10:32:36 -0300

    ainda não foi feito - energia solar

--- pilhas/exerc/energia.java
+++ pilhas/exerc/energia.java
@@ -6,6 +6,17 @@ import linkedList.NodeStack.Node;
 import linkedList.pilha;
 
 public class energia{
+    public class Node<E>{
+        private E element;
+        private Node<E> next;
+        public Node(){  element = null; next = null;}
+        public Node(E e){element = e;}
+
+        public void setElem(E e)    {element = e;}
+        public void setNext(Node<E> n)  {next = n;}
+        public E getElem()  {return element;}
+        public Node<E> getNext(){return next;}
+    }
     public static void main(String[] args){
        NodeStack<Integer> linhas = new NodeStack<Integer>();
        linhas.push(5);
@@ -16,6 +27,7 @@ public class energia{
        System.out.println(linhas.top());
        NodeStack<Integer> p = new NodeStack<Integer>();
        
+
         do{
 
             // linhas.last().getNext().getElem() ==> pegar o elemento do penúltimo item empilhado
