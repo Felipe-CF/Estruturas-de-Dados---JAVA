d0254b33ffd04f8fd4c3089958b35fdbbbcdab1b
Felipe-CF <prog.study.fcf@gmail.com>
2024-03-07 21:41:50 -0300

    meio perto de resolver

--- pilhas/exerc/energia.java
+++ pilhas/exerc/energia.java
@@ -3,36 +3,36 @@ import linkedList.NodeStack;
 
 public class energia{
     public static void main(String[] args){
-       NodeStack<Integer> linhas = new NodeStack<Integer>();
-       linhas.push(5);
-       linhas.push(7);
-       linhas.push(2);
-       linhas.push(6);
-       linhas.push(3);
-      // System.out.println(linhas);
-       NodeStack<Integer>.Node<Integer> cursor; // item da pilha
-       NodeStack<Integer> a = new NodeStack<Integer>();
-       NodeStack<Integer> b = new NodeStack<Integer>();
-       do{
-        System.out.println(linhas);
-        cursor = linhas.last();
-        while (cursor.getNext() != null) {
-            if(cursor.getElem() < cursor.getNext().getElem()){
-                a.push(cursor.getElem());
-                b.push(cursor.getNext().getElem());
-            }
-            cursor = cursor.getNext();
-        }
-        if(b.isEmpty()){ // se não for precisa fazer +1 linha
-            System.out.println(a);
-            break;
-        }
-        else{ // foi necessário fazer uma nova linha
-            System.out.println(b);
-            linhas = a;
-            b = null;
-        }
-       } while(!a.isEmpty());
+        int[] a = {3, 6, 2, 7, 5};
+       NodeStack<Object> p = new NodeStack<Object>();
+       NodeStack<Object> l = new NodeStack<Object>();
+       NodeStack<Object> r = new NodeStack<Object>();
+       p.push(5);
+       p.push(7);
+       p.push(2);
+       p.push(6);
+       p.push(3);
+//      NodeStack<Object>.Node<Object> cursor = p.last();
 
+        for(int i=1; i < a.length; i++){
+            // int e = a[i];
+            System.out.println(a[i]);
+            if(a[i] > a[i-1]){ // a casa atual tem que sair da linha
+                l.push(a[i]);
+            }
+            else{
+                r.push(a[i+1]);    
+            }
+        }
+        System.out.println(l);
+
+        // while(p.size() > 2){
+        //     Object o = p.pop();
+        //     if((int)o < (int)p.top())
+        //         r.push(o);
+        //     else
+        //         l.push(o);
+        // }
+        
     }
 }
