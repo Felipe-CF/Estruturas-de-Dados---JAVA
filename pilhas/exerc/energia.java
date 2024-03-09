ae1191672425ae8c4c6fded5a2f3aee7f9169e2e
Felipe-CF <prog.study.fcf@gmail.com>
2024-03-09 13:54:23 -0300

    Falta inverter a saída e tentar usar somente pilha - resposta encontrada

--- pilhas/exerc/energia.java
+++ pilhas/exerc/energia.java
@@ -1,38 +1,64 @@
 package exerc;
+import java.util.ArrayList;
 import linkedList.NodeStack;
 
 public class energia{
     public static void main(String[] args){
-        int[] a = {3, 6, 2, 7, 5};
-       NodeStack<Object> p = new NodeStack<Object>();
-       NodeStack<Object> l = new NodeStack<Object>();
-       NodeStack<Object> r = new NodeStack<Object>();
-       p.push(5);
-       p.push(7);
-       p.push(2);
-       p.push(6);
-       p.push(3);
-//      NodeStack<Object>.Node<Object> cursor = p.last();
-
-        for(int i=1; i < a.length; i++){
-            // int e = a[i];
-            System.out.println(a[i]);
-            if(a[i] > a[i-1]){ // a casa atual tem que sair da linha
-                l.push(a[i]);
+        ArrayList<Integer> a1 = new ArrayList<Integer>();
+        ArrayList<Integer> a2;  NodeStack<Integer> p;
+        // a1.add(3);
+        // a1.add(6);
+        // a1.add(2);
+        // a1.add(7);
+        // a1.add(5);
+        a1.add(8);
+        a1.add(6);
+        a1.add(10);
+        a1.add(4);
+        a1.add(1);
+        a1.add(5);
+        a1.add(3);
+ 
+        while(a1.size() != 0){
+            a2 = new ArrayList<Integer>(); p = new NodeStack<Integer>(); // inicia a2 e p
+            if(a1.size() == 1){
+                p.push(a1.getFirst());
             }
             else{
-                r.push(a[i+1]);    
+                for(int i = 1; i < a1.size();i++){
+                    if(a1.get(i) > a1.get(i-1)){
+                        if(a2.size() == 0){
+                            if(p.isEmpty()){
+                                a2.add(a1.get(i-1)); p.push(a1.get(i));
+                            }
+                            else{
+                                if(p.top() != a1.get(i-1))
+                                    p.push(a1.get(i-1));
+                                a2.add(a1.get(i)); 
+                            }
+                        }
+                        else{
+                            a2.add(a1.get(i));
+                        }
+                    }
+                    else{
+                        if(p.isEmpty()){
+                            p.push(a1.get(i-1));
+                        }
+                        p.push(a1.get(i));
+                    }
+                }
+                System.out.println(p);
+                if(a2.size() == 1){
+                    System.out.println(a2);
+                    break;
+                }
+                a1 = a2; // atualizo a referencia da linha orignal para os valores restantes que não foram
+                // destinadas a uma linha ainda
+            
             }
-        }
-        System.out.println(l);
+        }   
 
-        // while(p.size() > 2){
-        //     Object o = p.pop();
-        //     if((int)o < (int)p.top())
-        //         r.push(o);
-        //     else
-        //         l.push(o);
-        // }
         
     }
 }
